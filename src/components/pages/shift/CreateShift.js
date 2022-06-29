f1435a24b10a76f3e40659bcdfaefa3357099ea3
Joese <fransasisi7@gmail.com>
2022-06-30 01:02:04 +0700

    add menu shift

--- src/components/pages/shift/CreateShift.js
+++ src/components/pages/shift/CreateShift.js
@@ -1,123 +1,71 @@
-import Sidebar from "../../partials/Sidebar";
-
 function CreateShift() {
   return (
-    <div>
+    <>
       <main className="d-flex">
-        <Sidebar />
-        <div
-          className="b-example-divider b-example-vr"
-          style={{ height: 1000 }}
-        />
-        <div className="container">
-          <div className="mt-4 m-5">
-            <h3>Daftar Anggota Baru</h3>
-          </div>
-          <div className="card m-5">
-            <div className="card-body">
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
-                    Nama Lengkap
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="nama"
-                    id="nama"
-                  />
-                  <div id="emailHelp" className="form-text">
-                    We'll never share your email with anyone else.
-                  </div>
+        <div className="card m-5">
+          <div className="card-body">
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Kode Shift</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="kode_shift"
+                  id="kode_shift"
+                />
+                <div id="emailHelp" className="form-text">
+                  pengisian kode shift menggunakan awalan S dan huruf kapital,
+                  contoh <strong>' S001 '</strong>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Tempat Lahir
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    name="tempat_lahir"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Tanggal Lahir
-                  </label>
-                  <input
-                    type="date"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    name="tanggal_lahir"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Jenis Kelamin
-                  </label>
-                  <select
-                    class="form-select"
-                    aria-label="Default select example"
-                  >
-                    <option selected>--Jenis Kelamin--</option>
-                    <option value="laki - laki">Laki - Laki</option>
-                    <option value="perempuan">Perempuan</option>
-                  </select>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Alamat
-                  </label>
-                  <textarea
-                    type="text"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    name="alamat"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    No Telepon
-                  </label>
-                  <input
-                    type="tel"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    name="tanggal_lahir"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Alamat Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    name="tanggal_lahir"
-                  />
-                </div>
-                <div className="mb-3 form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="exampleCheck1"
-                  />
-                  <label className="form-check-label" htmlFor="exampleCheck1">
-                    Dengan ini data yang saya masukkan telah benar dan sesuai
-                    dengan data sebenarnya{" "}
-                  </label>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </form>
-            </div>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Mulai</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="mulai"
+                  name="mulai"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Selesai</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="selesai"
+                  name="selesai"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Lokasi</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="lokasi"
+                  name="lokasi"
+                />
+              </div>
+
+              <div className="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Dengan ini data yang saya masukkan telah benar dan sesuai
+                  dengan data sebenarnya{" "}
+                </label>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </main>
-    </div>
+    </>
   );
 }
 export default CreateShift;
--- src/components/pages/shift/Shift.js
+++ src/components/pages/shift/Shift.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import Sidebar from "../../partials/Sidebar";
+import CreateShift from "./CreateShift";
+import EditShift from "./EditShift";
 
 function Shift() {
   return (
@@ -10,26 +12,28 @@ function Shift() {
         <div className="container">
           <div className=" bg-light rounded-2 mt-3">
             <div className="container-fluid py-3">
-              <h1 className="display-5 fw-bold">Halaman Anggota</h1>
+              <h1 className="display-5 fw-bold">Halaman Shift</h1>
               <p className="col-md-8 fs-4">
                 Klik tombol dibawah jika ingin langsung generate jadwal{" "}
               </p>
               <table className="table table-hover">
                 <thead>
                   <tr style={{ textAlign: "center" }}>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">No</th>
+                    <th scope="col">Kode Shift</th>
+                    <th scope="col">Mulai</th>
+                    <th scope="col">Selesai</th>
+                    <th scope="col">Lokasi</th>
                     <th scope="col">Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>S001</td>
+                    <td>07.00</td>
+                    <td>09.00</td>
+                    <td>Planet</td>
                     <td>
                       <div className="d-flex justify-content-evenly">
                         <Link to="/anggota/detail">
@@ -38,13 +42,15 @@ function Shift() {
                           </div>
                         </Link>
 
-                        <a
-                          className="btn btn-primary btn-sm"
-                          href="/histories/<%= data.id %>/edit"
-                          role="button"
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target="#formEdit"
+                          data-bs-whatever="@mdo"
                         >
-                          <i className="bi-pencil-fill"></i> Edit
-                        </a>
+                          Edit
+                        </button>
                         <form
                           method="post"
                           action="/histories/<%= data.id %>?_method=DELETE"
@@ -62,61 +68,27 @@ function Shift() {
                   </tr>
                   <tr>
                     <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>S002</td>
+                    <td>09.00</td>
+                    <td>11.00</td>
+                    <td>Garden</td>
                     <td>
                       <div className="d-flex justify-content-evenly">
-                        <a
-                          className="btn btn-success btn-sm"
-                          href="/histories/<%= data.id %>"
-                          role="button"
+                        <Link to="/anggota/detail">
+                          <div className="btn btn-success btn-sm" role="button">
+                            <i className="bi-eye-fill"></i> Lihat Detail
+                          </div>
+                        </Link>
+
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target="#formEdit"
+                          data-bs-whatever="@mdo"
                         >
-                          <i className="bi-eye-fill"></i> Lihat Detail
-                        </a>
-                        <a
-                          className="btn btn-primary btn-sm"
-                          href="/histories/<%= data.id %>/edit"
-                          role="button"
-                        >
-                          <i className="bi-pencil-fill"></i> Edit
-                        </a>
-                        <form
-                          method="post"
-                          action="/histories/<%= data.id %>?_method=DELETE"
-                        >
-                          <button
-                            className="btn btn-danger btn-sm"
-                            type="submit"
-                            onclick="return confirm('apakah anda yakin hapus data ?');"
-                          >
-                            <i className="bi-trash-fill"></i> Hapus
-                          </button>
-                        </form>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>
-                      {" "}
-                      <div className="d-flex justify-content-evenly">
-                        <a
-                          className="btn btn-success btn-sm"
-                          href="/histories/<%= data.id %>"
-                          role="button"
-                        >
-                          <i className="bi-eye-fill"></i> Lihat Detail
-                        </a>
-                        <a
-                          className="btn btn-primary btn-sm"
-                          href="/histories/<%= data.id %>/edit"
-                          role="button"
-                        >
-                          <i className="bi-pencil-fill"></i> Edit
-                        </a>
+                          Edit
+                        </button>
                         <form
                           method="post"
                           action="/histories/<%= data.id %>?_method=DELETE"
@@ -134,9 +106,68 @@ function Shift() {
                   </tr>
                 </tbody>
               </table>
-              <Link to="/anggota/create">
-                <button className="btn btn-primary">Tambah</button>
-              </Link>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#formInput"
+                data-bs-whatever="@mdo"
+              >
+                Buat Data shift
+              </button>
+
+              <div
+                className="modal fade"
+                id="formInput"
+                tabIndex={-1}
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-lg">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Tambah Data Shift
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      />
+                    </div>
+                    <div className="modal-body">
+                      <EditShift />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="modal fade"
+                id="formEdit"
+                tabIndex={-1}
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-lg">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Edit Data Shift
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      />
+                    </div>
+                    <div className="modal-body">
+                      <CreateShift />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
